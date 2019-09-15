406c01698337cfd4901623f6071d5ad9a7b1b307
迟谋 <705359976@qq.com>
2019-09-15 11:25:05 +0800

    添加AI架构

--- Assets/_scripts/EnemyActor.cs
+++ Assets/_scripts/EnemyActor.cs
@@ -15,17 +15,101 @@ public class EnemyActor : MonoBehaviour
     #region Properties
     #endregion
     #region Private Methods 
-    void CheckAction()
+
+    #region base
+    float[] GetOrgWeights()
+    {
+        var _weights = new float[enemyInfo.baseActionTypeList.Count];
+        for (var i = 0; i < _weights.Length; i++)
+        {
+            _weights[i] = 100;
+        }
+        return _weights;
+    }
+    int GetNextAction()
+    {
+        var _weights = GetOrgWeights();
+        FindSomething(_weights);
+        MeetSomething(_weights);        
+        return NormalTool.GetRandomIndex(_weights, UnityEngine.Random.value);
+    }
+    void Action(int _index)
+    {       
+        if (_index < enemyInfo.baseActionTypeList.Count)
+        {
+            var _type = (BaseActionType)_index;
+            switch (_type)
+            {
+                case BaseActionType.Move:
+                    Move();
+                    break;
+                case BaseActionType.Rot90:
+                    Rot(90);
+                    break;
+                case BaseActionType.Rot180:
+                    Rot(180);
+                    break;
+                case BaseActionType.Jump:
+                    Jump();
+                    break;
+            }
+        }
+        else if(_index < enemyInfo.intermediateActionTypeList.Count)
+        {
+            var _type = (IntermediateActionType)_index;
+            switch (_type)
+            {
+                case IntermediateActionType.Jump:
+                    Jump();
+                    break;
+                case IntermediateActionType.LookAt:
+                    LookAt();
+                    break;
+            }
+        }
+        else
+        {
+            var _type = (AdvancedActionType)_index;
+            switch (_type)
+            {
+                case AdvancedActionType.Attack:
+                    Attack();
+                    break;
+                case AdvancedActionType.RunAway:
+                    RunAway();
+                    break;
+            }
+
+        }
+
+    }
+    #endregion
+
+
+    //在检测范围内发现了什么东西:某个行为的权重增加
+    void FindSomething(float[] _weights)
+    {
+    }
+    //触碰到了什么东西:某个行为的权重增加
+   void MeetSomething(float[] _weights)
     {
         
     }
-    #endregion
-    #region Utility Methods
-    public void Move()
+    void Move()
     {
 
     }
-    public void Rot()
+    void Rot(float _angle)
+    {
+
+    }
+    void Jump() { }
+    void Attack() { }
+    void LookAt() { }
+    void RunAway() { }
+    #endregion
+    #region Utility Methods
+    public void Create(EnemyInfo _enemyInfo,Vector3 _pos)
     {
 
     }
