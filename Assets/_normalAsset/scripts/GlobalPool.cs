17271ccb62087449bebac48dc32b6b63ffe02712
迟谋 <705359976@qq.com>
2019-09-16 08:58:31 +0800

    池

---
+++ Assets/_normalAsset/scripts/GlobalPool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 全局对象池： 包含多个子对象池，
+/// </summary>
+public class GlobalPool
+{
+    #region Parameters
+    private static GlobalPool instance;
+    public List<OnePool> onePools; 
+
+    #endregion
+    #region Properties
+    public static GlobalPool Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new GlobalPool();
+            return instance;
+        }
+
+        set
+        {
+            instance = value;
+        }
+    }
+    #endregion
+    #region Private Methods       
+    #endregion
+    #region Utility Methods
+    #endregion
+}
+public class OnePool
+{
+    public List<Object> pool;
+    public T GetObject<T>(Object _t)
+    {
+        if (pool.Contains(_t))
+        {
+
+        }
+        return default;
+    }
+}
---
+++ Assets/_scripts/EnemyManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    #region Parameters
+    string pathHeadOfScriptableObjects = "ScriptableObjects/";
+    string pathHeadOfPrefabs = "Prefabs/";
+    [HideInInspector]
+    public List<EnemyActor> enemyPool;
+    
+    #endregion
+    #region Properties
+    #endregion
+    #region Private Methods  
+
+    #endregion
+    #region Utility Methods
+    [ContextMenu("Load")]
+    void LoadTest()
+    {
+        LoadScriptableObject("Enemy00");
+    }
+    public EnemyInfo LoadScriptableObject(string _name)
+    {
+       return (EnemyInfo)Resources.Load(pathHeadOfScriptableObjects+_name);
+    }
+    public void CreateEnemy(string _name)
+    {
+
+        EnemyActor _actor = (EnemyActor)Resources.Load(pathHeadOfPrefabs + _name);
+    }
+    #endregion
+}
